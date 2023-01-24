02ba58bf869edad39118b64ad154efd5714532d5
Ryan Djemtchemeu <ryanmambou@gmail.com>
2023-01-24 18:49:38 +0100

    two sum problem

---
+++ T/TwoSum.js
@@ -0,0 +1,11 @@
+var twoSum = function(nums, target){
+    for(let i = 0; i < nums.length; i++){
+        for(let j = 1; j < nums.length; j++){
+            if(nums[i] + nums[j] == target){
+                if (i !==j){
+                    return [i, j]
+                }
+            }
+        }
+    }
+};
