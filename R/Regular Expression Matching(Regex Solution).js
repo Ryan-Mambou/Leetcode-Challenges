de7063f45305b00bf2fc657c8d3b6d9e63d49d38
Ryan Djemtchemeu <ryanmambou@gmail.com>
2023-02-08 08:16:46 +0100

    adding comments

--- R/Regular Expression Matching(Regex Solution).js
+++ R/Regular Expression Matching(Regex Solution).js
@@ -1,4 +1,5 @@
 var isMatch = function(s, p) {
+    //The caret(^) and dollar($) in quotes is to be able to avoid the pattern matching a string that is longer than it
  let pattern = new RegExp("^"+p+"$")
  return pattern.test(s);
 };
