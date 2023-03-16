d1d755941f3da75f301e8fbcdb1591aeff56a30a
Ryan Djemtchemeu <ryanmambou@gmail.com>
2023-03-16 05:44:01 +0100

    comments for 3sum

--- 0-9/3/3Sum.js
+++ 0-9/3/3Sum.js
@@ -1,3 +1,5 @@
+//Sort input, for each first element, find next two where -a = b+c, if a=prevA, skip a, if b=prevB skip b to elim duplicates; to find b,c use two pointers, left/right on remaining list;
+
 var threeSum = function(nums) {
     /** 
     nums[i] + nums[j] + nums[k] =  0
